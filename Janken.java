e25ea22df7d052d9c49b0af0fcaecb696b2273e4
tanaka-toshihiro <46895756+tanaka-toshihiro@users.noreply.github.com>
2019-02-27 18:49:30 +0900

    Update Janken.java

--- Janken.java
+++ Janken.java
@@ -10,48 +10,45 @@ public class Janken{
         int b = 3;
 	
         System.out.println("player 1");
-        while(a < 0 || a > 2 ){ 
+        do{
             System.out.println("0 = gu , 1 = tyoki , 2 = pa");	
-            a  = sc.nextInt();
-        }		   
+            a  = sc.nextInt();		
+        }while(a < 0 || a > 2 );       		   
         System.out.println("player1 "+str[a]);
          		 	 
         System.out.println("player 2");
-        while(b < 0 || b >2 ){ 
+        do{
             System.out.println("0 = gu , 1 = tyoki , 2 = pa"); 
             b  = sc.nextInt();		
-        }		   
+	}while(b < 0 || b >2 );		   
         System.out.println("player2 "+str[b]);           
 				   
         if(a==0){
             if(b==0){
-                System.out.println("drow");
+                System.out.println("draw");
             }else if(b==1){
-                System.out.println("a wins");		  
+                System.out.println("player1 wins");		  
             }else if(b==2){
-                System.out.println("b wins");
+                System.out.println("player2 wins");
             }
 	}	  
         if(a==1){
             if(b==1){
-                System.out.println("drow");
+                System.out.println("draw");
             }else if(b==2){
-                System.out.println("a wins");
+                System.out.println("player1 wins");
 	    }else if(b==0){
-                System.out.println("b wins");
+                System.out.println("player2 wins");
             }
 	}
         if(a==2){
             if(b==2){
-                System.out.println("drow");
+                System.out.println("draw");
             }else if(b==0){
-                System.out.println("a wins");
+                System.out.println("player1 wins");
             }else if(b==1){
-                System.out.println("b wins");
+                System.out.println("player2 wins");
             }
         } 
     }
-}	  
-	  
-	  
-	  
+}
